66f1cc28edd77cd00157cf171bff0573b13321d5
ChanPonleur <ponleurchan236@gmail.com>
2025-11-17 22:27:23 +0800

    Add files via upload

---
+++ 实验1第1道题.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i =  1; //将 Int 改成 int, 把 k 删掉
+	cout << i++ << endl;
+	//把第7行删掉
+	cout << i++ << endl;
+	cout << "Welcome to C++" << endl;
+	return 0;
+}
